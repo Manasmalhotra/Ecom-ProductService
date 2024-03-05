a223088efb4f3da50092b32d70d384b65b64a589
Manasmalhotra <manasmalhotra67@gmail.com>
2024-03-06 03:03:15 +0530

    Authentication Mechanism in Order Service

--- src/main/java/com/example/productservice/ProductServiceApplication.java
+++ src/main/java/com/example/productservice/ProductServiceApplication.java
@@ -3,9 +3,11 @@ package com.example.productservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ProductServiceApplication {
     @Bean
     public ModelMapper modelmapper(){
